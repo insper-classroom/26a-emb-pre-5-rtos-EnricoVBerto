53049456ccbe3ec1e70c8ebe458e93caeb8d3db4
EnricoVBerto <enricovberto@hotmail.com>
2026-04-15 16:24:54 -0300

    .

--- exe5/main.c
+++ exe5/main.c
@@ -17,9 +17,9 @@ const int LED_YELLOW_PIN = 10;
 #define BTN_RED_EVENT 1
 #define BTN_YELLOW_EVENT 2
 
-QueueHandle_t buttonQueue;
-SemaphoreHandle_t redLedSemaphore;
-SemaphoreHandle_t yellowLedSemaphore;
+QueueHandle_t xQueueBtn;
+SemaphoreHandle_t xSemaphoreLedR;
+SemaphoreHandle_t xSemaphoreLedY;
 
 void btn_callback(uint gpio, uint32_t events) {
     uint8_t btnEvent = 0;   
@@ -35,7 +35,7 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 
     if (btnEvent != 0) {
-        xQueueSendFromISR(buttonQueue, &btnEvent, NULL);
+        xQueueSendFromISR(xQueueBtn, &btnEvent, NULL);
     }
 }
 
@@ -48,7 +48,7 @@ void redLedTask(void *p) {
     int redLedState = 0;
 
     while (true) {
-        if (xSemaphoreTake(redLedSemaphore, 0) == pdTRUE) {
+        if (xSemaphoreTake(xSemaphoreLedR, 0) == pdTRUE) {
             redBlinkEnabled = !redBlinkEnabled;
             if (!redBlinkEnabled) {
                 redLedState = 0;
@@ -76,7 +76,7 @@ void yellowLedTask(void *p) {
     int yellowLedState = 0;
 
     while (true) {
-        if (xSemaphoreTake(yellowLedSemaphore, 0) == pdTRUE) {
+        if (xSemaphoreTake(xSemaphoreLedY, 0) == pdTRUE) {
             yellowBlinkEnabled = !yellowBlinkEnabled;
             if (!yellowBlinkEnabled) {
                 yellowLedState = 0;
@@ -112,20 +112,20 @@ void buttonTask(void *p) {
     gpio_set_irq_enabled(BTN_YELLOW_PIN, GPIO_IRQ_EDGE_FALL, true);
 
     while (true) {
-        if (xQueueReceive(buttonQueue, &receivedBtnEvent, portMAX_DELAY) == pdTRUE) {
+        if (xQueueReceive(xQueueBtn, &receivedBtnEvent, portMAX_DELAY) == pdTRUE) {
             if (receivedBtnEvent == BTN_RED_EVENT) {
-                xSemaphoreGive(redLedSemaphore);
+                xSemaphoreGive(xSemaphoreLedR);
             } else if (receivedBtnEvent == BTN_YELLOW_EVENT) {
-                xSemaphoreGive(yellowLedSemaphore);
+                xSemaphoreGive(xSemaphoreLedY);
             }
         }
     }
 }
 
 int main() {
-    buttonQueue = xQueueCreate(8, sizeof(uint8_t));
-    redLedSemaphore = xSemaphoreCreateBinary();
-    yellowLedSemaphore = xSemaphoreCreateBinary();
+    xQueueBtn = xQueueCreate(8, sizeof(uint8_t));
+    xSemaphoreLedR = xSemaphoreCreateBinary();
+    xSemaphoreLedY = xSemaphoreCreateBinary();
 
     xTaskCreate(redLedTask, "LED_Task 1", 256, NULL, 1, NULL);
     xTaskCreate(yellowLedTask, "LED_Task 2", 256, NULL, 1, NULL);
